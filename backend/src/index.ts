ba8e57064591e500db86c92126544d434fdaac35
SURAJARS <surajars24@gmail.com>
2026-09-12 10:13:52 +0530

    fixed index.ts cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: [
+      "https://price-seven-iota.vercel.app",
+      "http://localhost:3000"
+    ],
     credentials: true,
   })
 );
